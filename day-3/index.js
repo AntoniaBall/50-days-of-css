5e114b41a46647a90e599e420f007f976eb79c75
AntoniaBall <antonia.balluais@gmail.com>
2023-08-21 22:57:54 +0200

    day-3

---
+++ day-3/index.js
@@ -0,0 +1,6 @@
+
+function openOrCloseNav() {
+    // either remove or add the class name to container element
+    const container = document.querySelector('.container');
+    container.classList.toggle('show-nav');
+}
